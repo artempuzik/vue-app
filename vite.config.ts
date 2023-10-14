4a058b93d80f6d517fc749dda727064b83161ed9
Artem Puzik <artem.puzik.it@gmail.com>
2023-10-14 22:37:04 +0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,8 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  devServer: {
-    allowedHosts: 'all',
-  },
   server: {
     port: 5173,
-    proxy: {
-      '/': 'http://104.248.24.125:8000/api/'
-    },
     hot: true,
   },
 });
