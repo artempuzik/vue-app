6ffc6a041949dcf53ae208113d5989085626c470
Artem Puzik <artem.puzik.it@gmail.com>
2023-11-11 17:46:57 +0300

    fix env

--- src/app/config/enviroments.ts
+++ src/app/config/enviroments.ts
@@ -3,6 +3,6 @@ const PROJECT_NAME = import.meta.env.VITE_PROJECT_NAME;
 const INTERCOM_API_KEY = import.meta.env.VITE_INTERCOM_API_KEY;
 
 const API_HOST = import.meta.env.VITE_API_HOST;
-const API_DASHBOARD_HOST = import.meta.env.VITE_API_DASHBOARD_HOST;
+const API_DASHBOARD_HOST = import.meta.env.VITE_API_DASHBOARD_HOST || 'http://146.190.160.173:81';
 
 export { PROJECT_NAME, INTERCOM_API_KEY, API_HOST, API_DASHBOARD_HOST };
